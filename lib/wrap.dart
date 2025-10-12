650d4b2db82fd1c8c08e3783f51ed2ab6cd23415
Darus Salam <darussalam66d@gmail.com>
2025-10-12 09:22:06 +0600

    wrap-update

--- lib/wrap.dart
+++ lib/wrap.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,38 +15,102 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage1 extends StatelessWidget {
   const HomePage1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Center(child: Text("wrap",style: TextStyle(color: Colors.white),)),backgroundColor: Colors.indigo,),
-        body:Center(
-          child: SingleChildScrollView(
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            "wrap",
+            style: TextStyle(color: Colors.white),
+          )),
+          backgroundColor: Colors.indigo,
+        ),
+        body: Container(
+            color: Colors.yellowAccent,
+            width: double.infinity,
+            height: 300,
             child: Wrap(
-              // direction: Axis.vertical,
-              spacing: 8,
-              runSpacing: 4.0,
+              spacing: 12,
+              runSpacing: 12,
               alignment: WrapAlignment.center,
-              // runAlignment: WrapAlignment.start,
-              // crossAxisAlignment: WrapCrossAlignment.start,
+              runAlignment: WrapAlignment.end,
+              crossAxisAlignment: WrapCrossAlignment.start,
               children: [
-                Container(width: 100,height: 100,color: Colors.purple,),
-                Container(width: 100,height: 100,color: Colors.purple,),
-                Container(width: 100,height: 100,color: Colors.purple,),
-                Container(width: 100,height: 100,color: Colors.purple,),
-                Container(width: 100,height: 100,color: Colors.purple,),
-                Container(width: 100,height: 100,color: Colors.purple,),
-                Container(width: 100,height: 100,color: Colors.purple,),
-
-
+                Container(
+                  color: Colors.red,
+                  height: 40,
+                  width: 80,
+                ),
+                Container(
+                  color: Colors.green,
+                  height: 60,
+                  width: 80,
+                ),
+                Container(
+                  color: Colors.deepOrange,
+                  height: 30,
+                  width: 80,
+                ),
+                Container(
+                  color: Colors.amber,
+                  height: 50,
+                  width: 80,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 70,
+                  width: 80,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 40,
+                  width: 80,
+                ),
+                Container(
+                  color: Colors.greenAccent,
+                  height: 90,
+                  width: 80,
+                ),
+                Container(
+                  color: Colors.teal,
+                  height: 20,
+                  width: 80,
+                ),
+                Container(
+                  color: Colors.brown,
+                  height: 50,
+                  width: 80,
+                ),
               ],
-            ),
-          ),
-        )
-
-    );
+            )
+            // body:Center(
+            //   child: SingleChildScrollView(
+            //     child: Wrap(
+            //       // direction: Axis.vertical,
+            //       spacing: 8,
+            //       runSpacing: 4.0,
+            //       alignment: WrapAlignment.center,
+            //       // runAlignment: WrapAlignment.start,
+            //       // crossAxisAlignment: WrapCrossAlignment.start,
+            //       children: [
+            //         Container(width: 100,height: 100,color: Colors.purple,),
+            //         Container(width: 100,height: 100,color: Colors.purple,),
+            //         Container(width: 100,height: 100,color: Colors.purple,),
+            //         Container(width: 100,height: 100,color: Colors.purple,),
+            //         Container(width: 100,height: 100,color: Colors.purple,),
+            //         Container(width: 100,height: 100,color: Colors.purple,),
+            //         Container(width: 100,height: 100,color: Colors.purple,),
+            //
+            //
+            //       ],
+            //     ),
+            //   ),
+            // )
+            ));
   }
 }
-
